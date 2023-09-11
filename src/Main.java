d27ecbb4ae03a730d9ec4f34d80bad834437303d
Andrei Cernisov <andrei.cernisov@isa.utm.md>
2023-09-11 14:31:38 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+public class Main {
+    public static void main(String[] args) {
+
+        Member member = new Member("Don", 34, 9);
+        member.printCategory();
+
+        Member member1 = new Member("Don", 64, 3);
+        member1.printCategory();
+
+        Member member2 = new Member("Don",56, 9);
+        member2.printCategory();
+    }
+}
---
+++ src/Member.java
@@ -0,0 +1,29 @@
+public class Member {
+    private String name;
+    private int age;
+    private int handicap;
+    private Category category;
+
+    enum Category {OPEN, SENIOR}
+
+    public Member(String name, int age, int handicap) {
+        this.name = name;
+        this.age = age;
+        this.handicap = handicap;
+
+        this.category = assignCategory(age, handicap);
+    }
+
+    private Category assignCategory(int age, int handicap) {
+        if (age > 55 && handicap > 7) {
+            return Category.SENIOR;
+        } else {
+            return Category.OPEN;
+        }
+    }
+
+    public void printCategory() {
+        System.out.println(this.category);
+    }
+
+}
